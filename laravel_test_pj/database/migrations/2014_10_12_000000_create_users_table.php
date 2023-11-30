7892763e3cab85b13d230226249d760389aa3cf4
JunNaito <jun.development312@gmail.com>
2023-11-30 12:26:45 +0900

    user モデル定義、カラム追加

--- laravel_test_pj/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel_test_pj/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->tinyInteger('status')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+            $table->timestamps('deleated_at');
         });
     }
 
